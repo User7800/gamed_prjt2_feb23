0bb3bbf80b5e9e4ff8e5f7ad6c3150b916a0515c
jay-stiles <103208874+jay-stiles@users.noreply.github.com>
2023-03-06 16:48:46 -0600

    Slide Works!
    Death Slide Works! Like, it actually works!!

---
+++ ASSETS/Build/SlideR.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+onready var slide = $AnimationPlayer
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var kount = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	kount += 1
+	print("             SLIDEKOUNT = ", kount)
+	if(kount < 2):
+		body.position = $"../SpawnPoint".position
+		slide.play("Safe")
+	if (kount >= 2):
+		
+		print("EEEEEEEEEEEEEEENTERRRRRRRRR THE SLIIIIIIIIIIDEEEEEEEEEEEE")
+	#pass # Replace with function body.
