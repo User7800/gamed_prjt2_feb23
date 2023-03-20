4796f80b8c010802cb73f309b382e86211a11183
jay-stiles <103208874+jay-stiles@users.noreply.github.com>
2023-03-19 23:35:14 -0500

    not done endscreen
    and timer

---
+++ ASSETS/Menus/EndScreen.gd
@@ -0,0 +1,57 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var LevelOneTime = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_ReturnToMainMenu_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_Button_pressed():
+	pass # Replace with function body.
+
+
+
+func _on_Lvl1_pressed():
+	pass # Replace with function body.
+
+
+func _on_Lvl2_pressed():
+	pass # Replace with function body.
+
+
+func _on_Lvl3_pressed():
+	pass # Replace with function body.
+
+
+func _on_Lvl1_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_Lvl2_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_Lvl3_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_Quit_Button_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_ReturnToMainMenu_mouse_entered():
+	pass # Replace with function body.
---
+++ Maps/Timer.gd
@@ -0,0 +1,21 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var minSec = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	minSec += delta
+	var seconds = fmod(minSec, 60)
+	var minutes = fmod(minSec, 3600)/60
+	var passed = "%02d:%02d"% [minutes, seconds]
+	text = passed
+	#reference autoload for time storage
